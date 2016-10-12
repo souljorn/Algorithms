7c7709d1bb8df70c70e9ee5111782530bda82a89
Tim Botelho <timothybotelho@gmail.com>
2016-10-11 22:58:21 -0400

    Fixed functions with NullPtr Errors

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
@@ -15,163 +16,190 @@ class LinkedList
 {
 private:
 	Node* head;
+	Node* m_temp;
+	Node* m_current;
 	int listLength;
 public:
 
 	LinkedList();
-	void insert(Node* newNode, int index);
-	void remove(Node* pos, int data);
+	
+	
 	int pop();
 	void push(int data);
 	int getLength();
 	void setLength(int length);
-	Node* getPosition(int position);
-	Node* getHead();
+	int getNth(int position);
 	void printList();
 	bool isEmpty();
+	void deleteNode(int delData);
 	~LinkedList();
 };
 LinkedList::LinkedList()
 {
-	head = new Node;	
+	head = NULL;
+	m_temp = NULL;
+	m_current = NULL;
+	
 }
 LinkedList::~LinkedList()
 {
 }
 
-void LinkedList::insert(Node* iNode, int index)
-{
-	int length = getLength();
-	index = index + 1;//Due to offset of head not containing any data 
-	//check if index is less than zero or greater than listLength plus an
-	//added node
-	if (index < 1 || index > length + 1)
-	{
-		cout << " Please enter a valid index" << endl;
-	}
-	//check if it is the first node in the list
-	else if (isEmpty() && index == 1)
-	{
-		head->data = iNode->data;
-		
-	}
-	//traverse the list until the correct index is found
-	else
-	{
-		int count = 1;
-		Node* tempNode = getHead();	//Temp Node
-		Node* tailNode = getHead();	//Tail Node
-		while (count < index)
-		{
-			if (count == index)
-			{
-				tempNode->next = iNode;
-				iNode->next = tailNode;
-			}
-			//traverse temp to position in index
-			tempNode = tailNode;
-			tailNode = tempNode->next;
-			count++;
-		}
-	}
-}//func
+
 void LinkedList::push(int data)
 {
-	Node* iNode = new Node;
-	iNode->data = data;
-	Node* cNode;	//creator node 
-	Node* tNode = getHead();	//temp node
-	Node* tailNode = new Node;
-	tailNode = head->next;
-	// traverse to end of list
-	while (tailNode != NULL)
+	Node* n = new Node;
+	n->next = NULL;
+	n->data = data;
+
+	if (head != NULL)
 	{
-		tNode = tailNode;
-		tailNode = tNode->next;
+		m_current = head;
+		while (m_current->next != NULL)
+		{
+			m_current = m_current->next;
+		}
+		m_current->next = n;
 	}
-	tNode->next = iNode;	//Takes last element 
-	iNode->next = NULL;		// pointed at by temp and points to
-							// new element iNode and point inode->next to NULL
+	else
+	{
+		head = n;
+	}
 }
 
-Node* LinkedList::getHead() {
-	return this->head;
+int LinkedList::pop() {
+	Node* temp = head;
+	
+	int i = 0;
+	int length = getLength();
+	cout << length << endl;
+	while (i < length)
+	{
+		 temp = temp->next;
+	}
+	
+	int tempData = temp->data;
+	delete temp;
+
+	return tempData;
 }
 
-Node* LinkedList::getPosition(int position)
+int LinkedList::getNth(int position)
 {
-	Node* tempNode = getHead();
+	m_temp = head;
 	int count = 0;
-	if (position == 1) {
-		return tempNode;
+	if (position == 0) {
+		return m_temp->data;
 	}
-	else if ((position < 1) || (tempNode->next == NULL)) {
-		return NULL;
+	else if ((position < 0) || (m_temp == NULL)) {
+		return -1;
 	}
 	else {
 		while (position != count)
 		{
-			tempNode = tempNode->next;
+			m_temp = m_temp->next;
 			count++;
 			}
 		}
-		
-	return tempNode;
+	return m_temp->data;
 	}
 	
 void LinkedList::setLength(int length)
 {
 	listLength = length;
 }
+
 int LinkedList::getLength() {
-	Node* tempNode = getHead();
+	m_temp = head;
 	int count = 0;
-	while(tempNode != NULL)
+	while(m_temp != NULL)
 	{	
-		setLength(count++);
-		tempNode = tempNode->next;
+		setLength(++count);
+		m_temp = m_temp->next;
 	}
 	return listLength;
 }
 
 
 void LinkedList::printList() {
-	Node* tempNode = head->next;
-	if (head->next == NULL)
+	m_temp = head;
+	if (head == NULL)
 	{
 		cout << "List is empty" << endl;
 	}
 	else {
-		while (tempNode != NULL) {
-			cout << tempNode->data << endl;
-			tempNode = tempNode->next;
+		while (m_temp != NULL) {
+			cout << m_temp->data << endl;
+			m_temp = m_temp->next;
 		}
 	}
 }
 
 bool LinkedList::isEmpty()
 {
-	if (head->next== NULL)
+	if (head== NULL)
 		return true;
 	else
 		return false;
 }
+
+void LinkedList::deleteNode(int delData) {
+	Node* delPtr = NULL;
+	m_temp = head;
+	m_current = head;
+	while (m_current != NULL && m_current->data != delData)
+	{	
+		m_temp = m_current;
+		m_current = m_current->next;
+	}
+	if (m_current == NULL) {
+		cout << delData << "Was not in the list\n";
+		delete delPtr;
+	}
+	else {
+		delPtr = m_current;
+		m_current = m_current->next;
+		m_temp->next = m_current;
+		if (delPtr == head)
+		{
+			head = head->next;
+			m_temp = NULL;
+		}
+
+		delete delPtr;
+		cout << "The value " << delData << " was deleted \n";
+	}
+}
 int main() {
 	LinkedList list;
+	int numberOfNodes = 10;
 	cout << "Is List Empty: " << (list.isEmpty() ? "true" : "false")  << endl;
-	//Node* n1 = new Node;
-	//n1->data = 15;
-	//list.insert(n1, 0);
-	list.printList();
-	for (int i = 1; i < 10; i++)
-	list.push(i);
-	list.printList();
+	Node* n1 = new Node;
+	n1->data = 15;
 	
-	//list.insert(n1, 5);
+	list.printList();
+
+	int start_s = clock();
+	for (int i = 1; i < numberOfNodes; i++)
+	list.push(i);
+	int stop_s = clock();
+	cout << "time to create " << numberOfNodes << " nodes: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << " miliseconds"<<endl;
+	list.printList();
+	list.deleteNode(9);
+	list.deleteNode(8);
+	list.deleteNode(7);
+	list.deleteNode(1);
+
+	list.printList();
+	 start_s = clock();
+	list.printList();
+	 stop_s = clock();
+	cout << "time to print "<< numberOfNodes <<" nodes: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << " miliseconds" << endl;
+
 	list.printList();
 	cout << "List Length: "<<list.getLength() << endl;
-	Node* n = list.getPosition(4);
-	cout << "This is the data in position 4: "<< n->data << endl;
+	for (int i = 0; i < list.getLength(); i++)
+	cout << "The data in postion " << i << " is " << list.getNth(i) << endl;
 	system("pause");
 
 }
