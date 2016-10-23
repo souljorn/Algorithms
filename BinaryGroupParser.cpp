8c25a6bfd464d00feac19ec8406e9d406f4db5cf
[souljorn] <[souljorn@gmail.com]>
2016-10-23 17:47:44 -0400

    Fixed binary parser to take into account edge cases

--- BinaryGroupParser.cpp
+++ BinaryGroupParser.cpp
@@ -1,46 +1,41 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
-using namespace std;
-
-struct fragment{
-  int beginIndex;
-  int endIndex;
-  int size;
+struct Range {
+    int start;
+    int stop;
 };
-int main(int argc, char const *argv[]) {
-  /* code */
 
+int main() {
+    int arr[32] = { 1,1,1,1,1,1,1,1,
+                    1,1,1,1,1,1,1,1,
+                    0,0,0,0,0,0,0,0,
+                    0,0,0,0,0,0,0,0 };
 
-  int arr[32] = {0,1,1,1,1,0,0,0,
-                 1,1,1,0,0,0,0,0,
-                 1,1,0,1,0,0,0,1,
-                 0,0,0,0,0,1,1,1};
+    bool f = false;
+    Range r;
+    std::vector<Range> v;
 
-  vector<fragment> fragList;
+    for (int i = 0; i < 32; i++) {
+        if (!f && arr[i] == 1) {
+            f = true;
+            r.start = i;
+        }
 
-  int count = 0;
-  int freeCount = 0;
-  int memCount =0;
-  int begin = 0;
-  int end = 0;
-  int size = 0;
+        if (f && arr[i] == 0) {
+            f = false;
+            r.stop = i - 1;
+            v.push_back(r);
+        }
 
+        if (f && i == 31) {
+            f = false;
+            r.stop = i;
+            v.push_back(r);
+        }
+    }
 
-  while(count < 32){
-    while(count < 32 && arr[count] == 0) {count++;}
-    if(count < 32)
-    begin = count;
-    while(count < 32 && arr[count] ==1){ count++;}
-    end = count - 1;
-    size = end - begin;
-    fragment temp = {begin, end, size};
-    fragList.push_back(temp);
-  }
-  int i = 0;
-  while(i < fragList.size()){
-  cout << "Beginning Index: "<<fragList[i].beginIndex << " ";
-  cout << "End Index: " <<fragList[i++].endIndex << endl;
-}
-  return 0;
+    for (int i = 0; i < v.size(); i++)
+        std::cout << v[i].start << "   " << v[i].stop << "\n";
+
 }
